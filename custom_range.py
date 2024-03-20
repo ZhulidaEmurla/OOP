2a7411b8038d0c053394b200fdb269795e626d26
Zhulida Emurla <145500746+ZhulidaEmurla@users.noreply.github.com>
2024-03-21 00:54:00 +0100

    Add files via upload

---
+++ custom_range.py
@@ -0,0 +1,18 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.end:
+            result = self.current
+            self.current += 1
+            return result
+        else:
+            raise StopIteration
+
+
---
+++ reverse_string.py
@@ -0,0 +1,4 @@
+def reverse_text(text):
+    for char in reversed(text):
+        yield char
+
